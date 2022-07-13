736dbeb2f3e1d068f873a3d47d87c0642461d1e0
Noel Johnson <noel.johnson6213@gmail.com>
2022-07-13 18:35:12 +0530

    continue chapter 5

--- Chapter 05/django_project/settings.py
+++ Chapter 05/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-to26^ji4lrc0+nmjk#5r9ulj@c6)q1pt(x5lsiwvx)hc!43kgl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
